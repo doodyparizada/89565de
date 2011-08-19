3a9aebccd343c96971f826ab27ba983d7fd10905
mishraki <mishraki@mishraki-05475b>
2011-08-19 15:58:11 +0300

    initial version of SVM

--- 89565de/src/classifier/Classifier.java
+++ 89565de/src/classifier/Classifier.java
@@ -8,15 +8,19 @@ import java.util.List;
  */
 public interface Classifier {
 	/**
-	 * 
+	 *
 	 * @param features the features of a candidate entailment
 	 * @param doesEntail do these features match an entailments
 	 */
-	void learn(List<Feature> features, boolean doesEntail);
+	void addLearningExample(List<Feature> features, boolean doesEntail);
 	/**
-	 * 
+	 * crating a model made of the learning examples given
+	 */
+	void createModel();
+	/**
+	 *
 	 * @param features the feature list of a candidate entailment
-	 * @return true if the feature list belongs to an entailment, otherwise returns false 
+	 * @return true if the feature list belongs to an entailment, otherwise returns false
 	 */
 	boolean doesEntail(List<Feature> features);
 }
---
+++ 89565de/src/classifier/Svm.java
@@ -0,0 +1,66 @@
+package classifier;
+
+import java.net.MalformedURLException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+import jnisvmlight.FeatureVector;
+import jnisvmlight.LabeledFeatureVector;
+import jnisvmlight.SVMLightInterface;
+import jnisvmlight.SVMLightModel;
+import jnisvmlight.TrainingParameters;
+
+public class Svm implements Classifier {
+	SVMLightInterface trainer = new SVMLightInterface();
+	ArrayList<LabeledFeatureVector> traindata = new ArrayList<LabeledFeatureVector>();
+	SVMLightModel model;
+	public Svm() {
+		SVMLightInterface.SORT_INPUT_VECTORS = true;
+	}
+	@Override
+	public void addLearningExample(List<Feature> features, boolean doesEntail) {
+		LabeledFeatureVector featureVector = toFeatureVector(features,doesEntail);
+		traindata.add(featureVector);
+	}
+
+	@Override
+	public void createModel() {
+		// Initialize a new TrainingParamteres object with the default SVM-light
+	    // values
+	    TrainingParameters tp = new TrainingParameters();
+
+	    // Switch on some debugging output
+	    tp.getLearningParameters().verbosity = 1;
+
+	    System.out.println("\nTRAINING SVM-light MODEL ..");
+	    LabeledFeatureVector[] a=new LabeledFeatureVector[traindata.size()];
+		model = trainer.trainModel(traindata.toArray(a), tp);
+	    System.out.println(" DONE.");
+
+	    // Use this to store a model to a file or read a model from a URL.
+	    model.writeModelToFile("jni_model.dat");
+	    try {
+			model = SVMLightModel.readSVMLightModelFromURL(new java.io.File("jni_model.dat").toURI().toURL());
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public boolean doesEntail(List<Feature> features) {
+		FeatureVector featureVector = toFeatureVector(features,null);
+		double d = model.classify(featureVector);
+		return d < 0.0? false:true;
+	}
+	private LabeledFeatureVector toFeatureVector(List<Feature> features,Boolean lable) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
