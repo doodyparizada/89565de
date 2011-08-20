66d7df92d981def2acb823cf7de4f0ecc7c6d83f
mishraki <mishraki@mishraki-05475b>
2011-08-21 00:01:36 +0300

    almost nothing. (some lines in the main)

--- 89565de/src/main/Main.java
+++ 89565de/src/main/Main.java
@@ -1,5 +1,12 @@
 package main;
 
+import java.util.List;
+
+import classifier.Feature;
+
+import lex.SentenceEntailment;
+import parser.Parser;
+
 public class Main {
 
 	/**
@@ -8,9 +15,14 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// parse file
+		Parser parser = new Parser("Processed_DevSet.txt");
+		List<SentenceEntailment> sentenceEntailments = null;
 		// create SentenceEntailment Objects
-		// generate Features
-		// put in classifier
+		while(!(sentenceEntailments = parser.next()).isEmpty()){
+			// generate Features
+
+			// put in classifier
+		}
 		// output results
 	}
 
